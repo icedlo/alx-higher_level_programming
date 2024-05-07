4d0f440166182d02d20f3b50ff3da37c9a8e9f17
icedlo <munyaomusyimi10@gmail.com>
2024-05-07 12:06:41 -0700

    danadachidi

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const first = process.argv[2];
+const second = process.argv[3];
+
+function add (a, b) {
+  return a + b;
+}
+
+const a = parseInt(first);
+const b = parseInt(second);
+console.log(add(a, b));
