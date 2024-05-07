f893662183bb82219def796a620bf1acf1131235
icedlo <munyaomusyimi10@gmail.com>
2024-05-07 06:18:32 -0700

    t4

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -8,7 +8,7 @@ if (size === undefined || isNaN(parseInt(size))) {
   for (let i = 0; i < parseInt(size); i++) {
     let row = '';
     for (let j = 0; j < parseInt(size); j++) {
-      row += 'x';
+      row += 'X';
     }
     console.log(row);
   }
