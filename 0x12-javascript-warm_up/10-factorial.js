f3d4cec6970cfc54cfd2b9e9c17351a560250868
icedlo <munyaomusyimi10@gmail.com>
2024-05-07 12:24:07 -0700

    atieno

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let args = parseInt(process.argv[2]);
+if (isNaN(args)) {
+  args = 1;
+}
+function factorial (n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(args));
